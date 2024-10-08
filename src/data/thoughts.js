c0e6d21f9e7a69db0ee86563bc4610524d224b90
Keith Ratner <keith.ratner@gmail.com>
2024-10-08 15:17:11 -0500

    Messages

--- src/data/thoughts.js
+++ src/data/thoughts.js
@@ -1,9 +1,8 @@
 export const thoughts = [
-  "Fluid Dynamics",
+  "Fluid Dynamics would seem to dictate...",
   "Make mine an Old Fashioned.",
   "Keeping it simple.",
-  "I've been trying to channel my anger into something productive.",
-  "Your emotions are food for my soul.",
+  "All reality. ALL the time.",
   "Stop lookin' at me like that.",
   "My balls itch.",
 ];
