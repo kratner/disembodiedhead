10b9d6f93d1c9a33d009a98a21746a675da54759
Keith Ratner <keith.ratner@gmail.com>
2024-10-09 12:15:25 -0500

    Thoughts

--- src/data/thoughts.js
+++ src/data/thoughts.js
@@ -9,4 +9,6 @@ export const thoughts = [
   "I need to practice my scales.",
   "My singing voice ain't what it used to be.",
   "They're keeping me alive.",
+  "Once in a while, I get a hankering for smoked oysters.",
+  "It can be difficult being an empath, but it's better than being a sociopath. I think.",
 ];
